63a842dc6a052f9c02765338d044f3c9e1ac9bad
Mary-C-Eze <marypchinaza@gmail.com>
2025-02-27 00:29:26 -0500

    update app.py

--- app.py
+++ app.py
@@ -12,55 +12,35 @@ st.header(":blue[US Vehecle Data Base Analysis and Visualization]")
        
 
 st.write("*reading the csv into pandas df and viewing sample of the df.*")
-st.write(df_vehicles = pd.read_csv('../vehicles_us.csv'))
+df_vehicles = pd.read_csv('vehicles_us.csv')
 st.write (df_vehicles.sample(3))
 
 
-st.write ("*Exploring the df_vehicles.*")
-st.write (df_vehicles.info())
+df_vehicles ["is_4wd"] = df_vehicles['is_4wd'].fillna(0)
+df_vehicles.paint_color = df_vehicles.paint_color.fillna('unknown')
+df_vehicles = df_vehicles.dropna()
 
-st.write ("*the price is in normal format and no missing value. Model_year, cylinders, omometer, is_4wkd have some missing values; and type float will be converted to integer since the zeros have no relevance. No missing value in model, condition, transmission, type; and their format type object is good. date_posted will be converted to datetime format.*")
-st.write (df_vehicles['is_4wd'].unique())
+df_vehicles[['model_year', 'cylinders', 'odometer', 'is_4wd']] = df_vehicles[['model_year', 'cylinders', 'odometer', 'is_4wd']].astype(int)
 
-
-st.write ("*The nan value in column is_4wd will be converted to 0 (assuming that it is bool where 1 is yes and 0 is not 4wd).*")
-
-st.write (df_vehicles ["is_4wd"] = df_vehicles['is_4wd'].fillna(0))
-
-st.write (df_vehicles.paint_color = df_vehicles.paint_color.fillna('unknown'))
-
-st.write (df_vehicles = df_vehicles.dropna())
-
-
-st.write ("*Converting the columns data type from float to int*")
-
-st.write (df_vehicles[['model_year', 'cylinders', 'odometer', 'is_4wd']] = df_vehicles[['model_year', 'cylinders', 'odometer', 'is_4wd']].astype(int))
-
-st.write ("*extracting manufacturer from the model name*")
-
-st.write (df_vehicles['manufacturer'] = df_vehicles['model'].apply(lambda x: x.split()[0]))
+df_vehicles['manufacturer'] = df_vehicles['model'].apply(lambda x: x.split()[0])
 
 
 st.header("Disribution of the Day Listed")
 fig_1 = px.histogram(df_vehicles, x="days_listed")
-fig_1.show()
-st.write(fig_1)
+st.plotly_chart(fig_1)
 
 
 st.header("Disribution of the Vehecle Price")
 fig_2 = px.histogram(df_vehicles, x="price")
-fig_2.show()
-st.write (fig_2)
+st.plotly_chart (fig_2)
 
 st.header("Scatter plot Showing Correlation between Odometer and the Price")
 fig_3 = px.scatter(df_vehicles, x=df_vehicles['odometer'], y=df_vehicles["price"])
-fig_3.show()
-st.write (fig_3)
+st.plotly_chart (fig_3)
 
 st.header("Scatter plot Showing Correlation between days_listr and the Price")
 fig_4 = px.scatter(df_vehicles, x=df_vehicles['days_listed'], y=df_vehicles["price"])
-fig_4.show()
-st.write (fig_4)
+st.plotly_chart (fig_4)
 
 
 
