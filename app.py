a87e8b851155e0e2544c8fa489ea6f91ee13f8ca
Mary-C-Eze <marypchinaza@gmail.com>
2025-02-27 23:55:36 -0500

    updated the code

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import pandas as pd
-import scipy.stats
 import streamlit as st
-import time
 import plotly.express as px
-import numpy as np
-import plotly.figure_factory as ff
 
 
 
@@ -22,24 +18,45 @@ df_vehicles[['model_year', 'cylinders', 'odometer', 'is_4wd']] = df_vehicles[['m
 
 df_vehicles['manufacturer'] = df_vehicles['model'].apply(lambda x: x.split()[0])
 
+options = st.multiselect(
+    "What are your choice of manufacturers",
+    df_vehicles.manufacturer.unique().tolist(),
+    df_vehicles.manufacturer.unique().tolist()
 
-st.header("Disribution of the Day Listed")
-fig_1 = px.histogram(df_vehicles, x="days_listed")
+)
+
+st.write("You selected:", options)
+
+df_vehicles = df_vehicles.loc[df_vehicles.manufacturer.isin(options)]
+
+max_price_selected = st.slider("Range of Price",
+                        min_value=df_vehicles.price.min(),
+                        max_value = df_vehicles.price.max(),
+                        value=df_vehicles.price.max())
+st.write("Maximum Price Selected", max_price_selected)
+
+df_vehicles = df_vehicles.loc[df_vehicles.price <= max_price_selected]
+
+
+st.header("Distribution of the Day Listed")
+fig_1 = px.histogram(df_vehicles, x="days_listed", labels={"x": "days_listed", "y": "frequency"}, 
+                     color="manufacturer")
 st.plotly_chart(fig_1)
 
 
-st.header("Disribution of the Vehecle Price")
-fig_2 = px.histogram(df_vehicles, x="price")
+st.header("Distribution of the Vehecle Price")
+fig_2 = px.histogram(df_vehicles, x="price", labels={'x':'Price','y':'Frequency'}, color="manufacturer")
 st.plotly_chart (fig_2)
 
 st.header("Scatter plot Showing Correlation between Odometer and the Price")
-fig_3 = px.scatter(df_vehicles, x=df_vehicles['odometer'], y=df_vehicles["price"])
+fig_3 = px.scatter(df_vehicles, x=df_vehicles['odometer'], 
+                   y=df_vehicles["price"], color="manufacturer", trendline="ols")
 st.plotly_chart (fig_3)
 
 st.header("Scatter plot Showing Correlation between days_listr and the Price")
-fig_4 = px.scatter(df_vehicles, x=df_vehicles['days_listed'], y=df_vehicles["price"])
+fig_4 = px.scatter(df_vehicles, x=df_vehicles['days_listed'], 
+                   y=df_vehicles["price"], color="manufacturer", trendline="ols" )
 st.plotly_chart (fig_4)
-
 
 
 agree = st.checkbox("Acceped")
